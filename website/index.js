42f4d7fd25d16cf262b24ff4fbf6585a2aa77682
ShakeelGadafi <shakeelgadhafi@gmail.com>
2025-03-17 21:50:15 +0530

    dis:got the number in a grid of 4

---
+++ website/index.js
@@ -0,0 +1,31 @@
+//console.log('Hello, world!');
+//console.log('king shark');
+
+//window.alert('this is an alert');
+//windows.alert('Shakeel is king ');
+
+
+//this is a comment
+
+/* this 
+is 
+a c
+omment
+*/
+
+
+// these are logical relationship
+/*&& and 
+|| or
+! not
+*/
+
+// this are about the data types
+//string    "hello"
+//number    1, 2, 3, 4, 5
+//boolean   true, false
+//null      null
+//undefined undefined
+//object    {key: value}
+//array     [1, 2, 3, 4, 5]
+//function  function() {}
